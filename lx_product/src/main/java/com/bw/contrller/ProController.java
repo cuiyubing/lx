75ec03669cf65f76bc7c2f5529a123ba95da2073
bingbing <1565570355@qq.com>
2019-11-20 11:35:24 +0800

    再次提交2

--- lx_product/src/main/java/com/bw/contrller/ProController.java
+++ lx_product/src/main/java/com/bw/contrller/ProController.java
@@ -1,16 +1,19 @@
 package com.bw.contrller;
 
+import com.bw.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
-
 @RestController
 @RequestMapping("product")
 public class ProController {
+    @Autowired
+    private UserService userService;
     @RequestMapping("/user/list")
     public List<String> getList(){
-        return Arrays.asList("张三","李四","王五") ;
+        return userService.getList();
     }
 }
